f26fdc2f2eacfaf3b15be8dc8f41fe2f84aa857d
Tristan <tristanlingat@gmail.com>
2022-08-30 12:36:53 -0700

    add foundational assets

---
+++ app.js
@@ -0,0 +1,13 @@
+//Author: Tristan Lingat
+//Date: July 5, 2022
+
+const express = require("express")
+const app = express()
+const port = 8080
+const path = require('path')
+
+app.get('', (req, res) => {
+    res.sendFile(path.join(__dirname, '/templates/index.html'))
+})
+
+app.listen(port, () => console.log('Listening on port 8080'))
