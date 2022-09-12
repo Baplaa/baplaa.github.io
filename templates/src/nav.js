c1e6b9cd0d9415094a0f962c86795e933b4843e4
Inspiron <tristanlingat@gmail.com>
2022-09-12 15:03:25 -0700

    break header name

--- templates/src/nav.js
+++ templates/src/nav.js
@@ -3,7 +3,7 @@ function navs() {
     nav_div.insertAdjacentHTML("beforebegin",
     `
         <nav>
-            <h1 class="header_name">TRISTAN LINGAT</h1>
+            <h1 class="header_name">TRISTAN<br>LINGAT</h1>
             <ul class="nav_link_list">
             <li class="nav_link">
                 <a href="#insight" class="nav_link_a">
